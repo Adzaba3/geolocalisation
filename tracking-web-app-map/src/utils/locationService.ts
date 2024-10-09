2633bb76cd24cf24bb6ceb75f90d446077697096
i-kewil <kndjeyi@mail.connecttechnology.io>
2024-10-09 10:40:52 +0100

    modification for the name of the map and IP adress

--- tracking-web-app-map/src/utils/locationService.ts
+++ tracking-web-app-map/src/utils/locationService.ts
@@ -1,17 +1,17 @@
 // src/services/locationService.ts
-import axios from 'axios';
+import axios from 'axios'
 
-const API_URL = 'http://localhost:5001/api/get-locations'; // Remplacer par l'URL de ton serveur AWS
+const API_URL = 'http://13.51.175.43:5001/api/get-locations' // Remplacer par l'URL de ton serveur AWS
 
 // Fonction pour récupérer les données de localisation
 export const getLocations = async () => {
   try {
-    const response = await axios.get(API_URL);
+    const response = await axios.get(API_URL)
     console.log(response.data.data)
 
-    return response.data.data; // Supposons que les données sont dans response.data.data
+    return response.data.data // Supposons que les données sont dans response.data.data
   } catch (error) {
-    console.error('Erreur lors de la récupération des données de localisation:', error);
-    throw error;
+    console.error('Erreur lors de la récupération des données de localisation:', error)
+    throw error
   }
-};
+}
