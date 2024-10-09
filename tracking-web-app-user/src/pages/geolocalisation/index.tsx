ea8383664c8d96ba443d3d6d12f3e16a2bf05dbb
i-kewil <kndjeyi@mail.connecttechnology.io>
2024-10-09 15:01:42 +0100

    mettre pour l appareil a tracker sur le server distant AWS

--- tracking-web-app-user/src/pages/geolocalisation/index.tsx
+++ tracking-web-app-user/src/pages/geolocalisation/index.tsx
@@ -50,7 +50,7 @@ const GPSComponent: React.FC = () => {
   const sendLocationToServer = useCallback(async (location: Location) => {
     if (location.latitude !== null && location.longitude !== null) {
       try {
-        await fetch('https://mon-backend-aws.com/api/save-location', {
+        await fetch('http://13.51.175.43:5001/api/save-location', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
