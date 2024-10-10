425f1faef695161da3693921a1a2ca6c712d837a
i-kewil <kndjeyi@mail.connecttechnology.io>
2024-10-10 16:01:47 +0100

    pour les requetes en CORS

--- tracking-app/app.js
+++ tracking-app/app.js
@@ -13,9 +13,30 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+
+// Liste des origines autorisées
+const allowedOrigins = [
+  'https://security-tracker.smartds.io/',
+  'http://95.111.225.198:5000/',
+  // 'http://localhost:5000', // Par exemple, si tu veux autoriser le développement en local
+  // 'http://localhost:5002', 
+];
+
+
 app.use(cors({
   // origin: 'http://13.51.175.43:5000',  // Remplace par l'origine de ton application React (URL où l'app est hébergée)
-  origin: '*',  // Remplace par l'origine de ton application React (URL où l'app est hébergée)
+  // origin: '*',  // Remplace par l'origine de ton application React (URL où l'app est hébergée)
+  origin: function (origin, callback) {
+    // Si aucune origine (comme dans une requête serveur-serveur), autoriser
+    if (!origin) return callback(null, true);
+    // Si l'origine est dans la liste, autoriser
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    // Sinon, rejeter la requête
+    return callback(new Error('Accès interdit par la politique CORS'));
+  },
   methods: ['GET', 'POST'], // Méthodes HTTP autorisées
   credentials: true, // Si tu utilises des cookies ou des sessions
 }));
